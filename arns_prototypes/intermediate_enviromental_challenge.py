ca1dceed125ddb90d04c147ba9eaeb3702629c9c
Gustavo Valenzuela <111663500+GustavoVaAsc@users.noreply.github.com>
2024-09-21 12:36:03 -0600

    Deleted arns_prototypes directory

--- arns_prototypes/intermediate_enviromental_challenge.py
+++
@@ -1,5 +0,0 @@
-from enviromental_challenge import enviromental_challenge
-class intermediate_enviromental_challenge(enviromental_challenge):
-  def __init__(self, name, description, videogame_daily_time_reward=1800, videogame_full_time_reward=14400, days_to_complete=21, cur_days=0):
-    super().__init__(name, description, videogame_daily_time_reward, videogame_full_time_reward, days_to_complete)
-  
